4f35f6ade0841230e4b79402b4ad35ae3fa1432f
akmil <pashades@gmail.com>
2015-03-12 14:08:53 +0200

    decrease change -2

--- pizza_pictureviewer/js/pizza_coustructor/pizzaConstructorCtrl.js
+++ pizza_pictureviewer/js/pizza_coustructor/pizzaConstructorCtrl.js
@@ -99,19 +99,24 @@ function init(){
     
     $scope.decreaseItem = function (_price, _id, _name ,_radioModel , _quantity){
         //check if exist element for decrease 
-        bool = false;  
+        ///bool = false;  
+        var found = false;
+        
         console.log('_$scope.finalPriceModel.length',$scope.finalPriceModel.length);
         for (i = 0; i < $scope.finalPriceModel.length; i++) {
-                if (_name !== $scope.finalPriceModel[i].name) {                
-                    //console.log(_name + '_name found',_name !== $scope.finalPriceModel[i].name);                                    
-                      
-                    return;                    
-                    }else break;
-        }
+                console.log('i',i);
+                if (_name === $scope.finalPriceModel[i].name) {                
+                    console.log(_name + '_name not found',_name !== $scope.finalPriceModel[i].name);
+                    found = !found;
+                    break;                    
+                    }
+            }
+         if(!found)return;
+         
          if (_radioModel === 'Full'){
              _price = _price * 1;
          }//else _price = _price * 0.5;
-         isArrElem(_name, _radioModel,bool);//false: is quantity--
+         isArrElem(_name, _radioModel,false);//false: is quantity--
         if (!bool) {
             //push new val of quantity
             $scope.finalPriceModel.push({ name: _name, id: _id ,price: _price , radioModel: _radioModel, quantity : _quantity}); 
