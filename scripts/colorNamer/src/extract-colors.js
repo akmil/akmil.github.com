87b0f69b7fbaa66352a8f88bd8fa65094de68029
Pavlo Oliinyk <pashades@gmail.com>
2017-11-02 14:56:07 +0200

    linter fix

--- scripts/colorNamer/src/extract-colors.js
+++ scripts/colorNamer/src/extract-colors.js
@@ -5,7 +5,7 @@ import {ntc} from './nameThatColor.js';
  */
 var patt1 = /#[a-zA-Z0-9]{6}|rgb\((?:\s*\d+\s*,){2}\s*[\d]+\)|rgba\((\s*\d+\s*,\s*){3}[\d\.]+\)|hsl\(\s*\d+\s*(\s*\,\s*\d+\%){2}\)|hsla\(\s*\d+(\s*,\s*\d+\s*\%){2}\s*\,\s*[\d\.]+\)/gm;
 var testCss = '"#0D2E1C", "#2F4F4F", "#4F9D5D", "#FFFF31", "#7FFFD4", "#FFEFC1"';
-var getTextColor =function (string, patt1) {
+var getTextColor = function (string, patt1) {
   return string.match(patt1);
 };
 var outColor = getTextColor(testCss, patt1);
@@ -33,13 +33,13 @@ function addCustomList(outColor, className) {
   let $ul = $('<ol class="' + className + ' custom_select__list">').appendTo($listWrapper), // create ul
     colorsSet = [],
     setRGBA = (item)=> {
-      let val, //arr or string
+      let val, // arr or string
         isHEX = item.includes('#'),
         isRGBA = item.includes('rgba' || 'RGBA'),
         isRGB = item.includes('rgb' || 'RGB');
 
       if (isHEX) {
-        //corol is HEX, convert to rgb
+        // corol is HEX, convert to rgb
         val = ntc.rgb(item);
         val[3] = (ntc.name(item)[3] === false) ? -1 : ntc.name(item)[3];
       }
@@ -58,7 +58,7 @@ function addCustomList(outColor, className) {
         isRGB = item.includes('rgb' || 'RGB');
 
       if (isHEX) {
-        //corol is HEX, convert to rgb
+        // corol is HEX, convert to rgb
         val = ntc.rgb(item);
       }
       if (isRGBA || isRGB) {
@@ -70,16 +70,16 @@ function addCustomList(outColor, className) {
       return val;
     };
 
-  //put li inside ul
+  // put li inside ul
   outColor
     .map(function (item) {
       var rgb = ntc.rgb(item),
         n_match = ntc.name(item),
         name = n_match[1],
-        isRGB = item.includes('rgb' || 'rgba' || 'RGB' || 'RGBA' &&'('&&')'),
+        isRGB = item.includes('rgb' || 'rgba' || 'RGB' || 'RGBA' && '(' && ')'),
         strRGB = ' RGB:',
-        _hex,_rgb,_rgba,_hls;
-      var colorsOnly ,red , green, blue;
+        _hex, _rgb, _rgba, _hls;
+      var colorsOnly, red, green, blue;
 
       if (isRGB) {
         strRGB = ' ';
@@ -88,22 +88,22 @@ function addCustomList(outColor, className) {
         green = colorsOnly[1];
         blue = colorsOnly[2];
 
-        ( item.includes('rgba' || 'RGBA' ) )
+        (item.includes('rgba' || 'RGBA'))
           ? rgb = item
-          : rgb = ntc.rgb2hex(red,green,blue);
+          : rgb = ntc.rgb2hex(red, green, blue);
       }
       
-      _hex = (item.includes('#')) ? item : ntc.rgb2hex(red,green,blue);
+      _hex = (item.includes('#')) ? item : ntc.rgb2hex(red, green, blue);
       _rgb = setRGB(item);
       _rgba = setRGBA(item);
 
-      //create output colors obj
+      // create output colors obj
       colorsSet.push({
         name,
-        opacity:(n_match[3] === -1)?false:n_match[3],
+        opacity: (n_match[3] === -1) ? false : n_match[3],
         hex: _hex,
         rgb: _rgb,
-        rgba:_rgba,
+        rgba: _rgba,
         hls: 'not supported'
       });
 
@@ -114,7 +114,7 @@ function addCustomList(outColor, className) {
             <span><b> Name: ${name}</b></span>
             </p>
         </li>`
-      )
+      );
     });
 
   $ul.after(
@@ -123,7 +123,7 @@ function addCustomList(outColor, className) {
 
   const scssBtnHandler = ()=> {
     var $wrapper = $('.js__list-resulting-scss'),
-      $ulSCSS = $('<ul class="list-group prepared">').appendTo($wrapper);//create ul
+      $ulSCSS = $('<ul class="list-group prepared">').appendTo($wrapper); // create ul
 
     if ($('.list-group.prepared').length > 1) {
       $('.list-group.prepared').get(0).remove();
@@ -131,16 +131,17 @@ function addCustomList(outColor, className) {
 
     colorsSet
       .map(function (item) {
-        let showVal = (item.opacity === false ) ? item.hex : `rgba(${item.rgba})`;
+        let showVal = (item.opacity === false) ? item.hex : `rgba(${item.rgba})`;
+
         $ulSCSS.append(
           `<li class='custom_select__item list-unstyled'>            
               $${Case.toSnakeCase(item.name.toLowerCase())}: ${showVal};            
         </li>`
-        )
+        );
       });
   };
 
-  $('.js_to-scss').on('click',scssBtnHandler);
+  $('.js_to-scss').on('click', scssBtnHandler);
 }
 
 export function createCustomList() {
@@ -155,10 +156,9 @@ export function createCustomList() {
   addCustomList(colorsArr, 'named-colors-container');
 }
 
-
 var Case = {
-  titleToCamelCase : function (string) {
-      if( typeof dashed != "string" || dashed.length==0 )
+  titleToCamelCase: function (dashed) {
+      if(typeof dashed != "string" || dashed.length == 0)
         return "";
       return dashed.toLowerCase().replace(/\s([a-z]?)/g, function(match, letter) {
         return letter.toUpperCase();
@@ -171,11 +171,11 @@ var Case = {
       return (pos > 0 ? "-" : "") + letter.toLowerCase();
     });
   }, */
-  toSnakeCase : function (string) {
+  toSnakeCase: function (string) {
     string = string.toLowerCase();
-    return string.replace(/(\s\w)/g, function(m){return "-"+m[1].toLowerCase();});
+    return string.replace(/(\s\w)/g, function(m){return "-" + m[1].toLowerCase();});
   },
-  snakeCaseToCamelCase : function( dashed ) {
+  snakeCaseToCamelCase: function(dashed) {
     if( typeof dashed != "string" || dashed.length==0 )
       return "";
     return dashed.toLowerCase().replace(/\-([a-z]?)/g, function(match, letter) {
