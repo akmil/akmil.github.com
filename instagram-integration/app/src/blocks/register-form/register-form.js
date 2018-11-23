5fdf1aa267b7c1e1eb9169b9444935abeca1c2cb
Pavlo_Oliinyk1 <Pavlo_Oliinyk1@epam.com>
2018-11-23 19:04:20 +0200

    add: error handler

--- instagram-integration/app/src/blocks/register-form/register-form.js
+++ instagram-integration/app/src/blocks/register-form/register-form.js
@@ -1,5 +1,6 @@
 import $ from 'jquery';
 import User from '../../common/js-services/user';
+import {CONST} from '../../common/js-services/consts';
 // import {CONST} from '../../common/js-services/consts';
 
 export default class RegisterForm {
@@ -22,14 +23,31 @@ export default class RegisterForm {
         this.$email.val(this.$email.val().toLocaleLowerCase());
         this.formData = formDataObj || {email, password};
 
-        this.user.register(this.formData).then((result) => {
-            console.log(result);
-            this.$textAreaDescription.text(`succsess, status ${result.status.state}`);
-            $('.login-box').show();
-        }).catch(function(error) {
-            console.log('request failed', error);
-            console.log('do something');
-        });
+        this.user.register(this.formData)
+            .then((result) => {
+                if (result.data && result.data.token) {
+                    CONST.user = {
+                        email: this.formData.email,
+                        password: this.formData.password,
+                        token: result.data.token
+                    };
+                    console.log('request succeeded with JSON response', result);
+                } else {
+                    this.$textAreaDescription
+                        .append(`<p>status: ${result.status.state}</p>`)
+                        .append(`<p> message: ${result.status.message} </p>`);
+                }
+            }).then((result) => {
+                if (result && result.status) {
+                    console.log(result);
+                    this.$textAreaDescription.text(`succsess, status ${result.status.state}`);
+                    $('.login-box').show();
+                }
+            }).catch((error) => {
+                console.log('request failed', error);
+                this.$textAreaDescription.text(error.message);
+                console.log('do something');
+            });
     }
 
     bindEvents() {
