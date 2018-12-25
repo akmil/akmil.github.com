471c21a88dc630c79cd19ba3e7abcd86c4317cc5
Pavlo Oliinyk1 <pavlo_oliinyk1@epam.com>
2018-12-25 17:03:54 +0200

    upd

--- instagram-integration/app/webpack/common.config.js
+++ instagram-integration/app/webpack/common.config.js
@@ -72,7 +72,6 @@ module.exports = {
     // })
     new ProvidePlugin({ $: 'jquery',
         jQuery: 'jquery',
-        'window.jQuery': 'jquery',
         Popper: ['popper.js', 'default']
     })
     
