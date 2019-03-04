e3718b6d44bd6bb5e5a8c72adf4e567ef5f16e19
Pavlo_Oliinyk1 <Pavlo_Oliinyk1@epam.com>
2019-03-04 18:07:09 +0200

    upd: form validation

--- instagram-integration/app/src/blocks/instagram-accounts-list/accounts-list-btn-settings.js
+++ instagram-integration/app/src/blocks/instagram-accounts-list/accounts-list-btn-settings.js
@@ -3,15 +3,19 @@ import {CONST} from '../../common/js-services/consts';
 
 export function settingButtonsHandler(classCfg) {
     const {deleteBtnCls} = classCfg;
-    // ендпоинт:
+    const modalConfirm = $('#delete-user-promt');
+    let username = '';
     // DELETE blablabla/instagram-accounts/{username}
-    // как всегда, токен в хидер
     $(deleteBtnCls).on('click', (e) => {
-        const username = $(e.target).closest(deleteBtnCls).data('username');
+        username = $(e.target).closest(deleteBtnCls).data('username');
         console.log();
+        modalConfirm.modal('show');
+    });
+    $('.js_acc-delete-confirm').on('click', (e) => {
         User.delInstagramAccount(username).then((result) => {
             console.log(result);
             if (result.status.state === 'ok') {
+                modalConfirm.hide();
                 window.PubSub.publish(CONST.events.instagramAccouns.INSTAGRAM_ACCOUNS_NEED_REFRESH);
             }
         });
