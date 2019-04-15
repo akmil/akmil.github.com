6906f4cef5269a9e222cf6550a35c49b2f4f593d
Pavlo Oliinyk1 <pavlo_oliinyk1@epam.com>
2019-04-15 21:32:56 +0300

    upd

--- instagram-integration/app/webpack/prod.config.js
+++ instagram-integration/app/webpack/prod.config.js
@@ -62,7 +62,7 @@ module.exports = webpackMerge(webpackCommon, {
     }),
     new ExtractTextPlugin('[name].css'), // -[chunkhash].min
     /* todo: turnOn UglifyJsPlugin for production */
-    /*
+    
     new UglifyJsPlugin({
       compressor: {
         screw_ie8: true,
@@ -81,7 +81,7 @@ module.exports = webpackMerge(webpackCommon, {
       },
       sourceMap: false // todo: set to FALSE for production
     }),
-    */
+    
     new LoaderOptionsPlugin({
       options: {
         context: '/',
