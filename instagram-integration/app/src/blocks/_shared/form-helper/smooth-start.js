af3d2d89681d7f82917ee827752200830ea8ed3f
Pavlo_Oliinyk1 <Pavlo_Oliinyk1@epam.com>
2019-06-26 14:52:43 +0300

    upd

--- instagram-integration/app/src/blocks/_shared/form-helper/smooth-start.js
+++ instagram-integration/app/src/blocks/_shared/form-helper/smooth-start.js
@@ -8,15 +8,20 @@ export function addSmoothStart(defaultCfg, state) {
     $('.js_smooth-starting').removeClass('d-none');
 
     $('.js_smooth-starting').on('change', (e) => {
-        // console.log(e.target.checked, smooth_starting);
-        state.user_default_config.smooth_starting_enabled = e.target.checked;
         if (!e.target.checked) {
             $('.js_toast-smooth-start').toast('show');
+            e.target.checked = true;
         }
+        state.user_default_config.smooth_starting_enabled = e.target.checked;
     });
     $('.js_not-confirm').on('click', (e) => {
         console.log('click');
         console.log(e.target);
         $('.js_smooth-starting #smooth_starting').prop('checked', true);
+    });
+    $('.js_confirm').on('click', (e) => {
+        console.log('click');
+        console.log(e.target);
+        $('.js_smooth-starting #smooth_starting').prop('checked', false);
     });
 }
